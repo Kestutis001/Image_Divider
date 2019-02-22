bf3d98214e170d448171bf863c16df2e7ff0ed96
kestutis saltonas <kestutis@pdx.edu>
2019-02-22 15:30:09 -0800

    Set up and test Emgu.

---
+++ Code/Image_Divider/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace Image_Divider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IMG_PATH = "C:\\Users\\Kestutis\\Documents\\Paintings\\IMG_4073.jpg";
+            var img = new Mat();
+
+            img = CvInvoke.Imread(IMG_PATH, ImreadModes.Color);
+
+
+            CvInvoke.NamedWindow("Image", NamedWindowType.Normal);
+            CvInvoke.Imshow("Image", img);
+            CvInvoke.WaitKey();
+            CvInvoke.DestroyAllWindows();
+        }
+    }
+}
